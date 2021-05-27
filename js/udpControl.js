034e0da54b05ccb45f5f1a5b1a3a44e17a8c4074
Liam Benedetti <libe@it-tf.local>
2021-05-27 15:50:51 +0200

    udpControls

--- js/udpControl.js
+++ js/udpControl.js
@@ -2,9 +2,14 @@ const io = require('./ioServer')
 const ucon = require("./ucon")
 
 const server = ucon.server("0.0.0.0", 4000)
+var currentControls = {}
 
 io.on("connection" ,(client)=>{
     client.on("car-control", (data)=>{
-        server.broadcast(JSON.stringify(data))
+        if (JSON.stringify(currentControls) != JSON.stringify(data)) {
+            console.log(data);
+            server.broadcast(JSON.stringify(data))
+            currentControls = data
+        }
     })
 })
