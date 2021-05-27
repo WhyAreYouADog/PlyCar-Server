41f7f6508788772e5f5c1d3abb7ec9740d9d7602
Andrin Schranz <ansc@it-tf.local>
2021-05-27 13:34:22 +0200

    new frontend

--- frontend/src/config/io.js
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client"
-const host = "http://64.227.115.0/"
-//64.227.115.0
-//192.168.60.91
-export const socket = io(host)
--- frontend/src/gamepadInput.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("gamepadconnected", (event) =>{
-    window.requestAnimationFrame()
-})
-
-var loop = ()=>{
-    var gamepad = navigator.getGamepads()[1]
-
-    console.log(gamepad);
-}
